cf798603c9f88e852be7843b28ffa3133947df44
okiogutu <okiogutu@gmail.com>
2024-06-20 12:02:53 +0000

    saved work

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,21 @@
+const Utils = {
+	
+  calculateNumber(type, a, b) {
+    switch (type) {
+      case 'SUM':
+        return Math.round(a) + Math.round(b);
+      case 'SUBTRACT':
+        return Math.round(a) - Math.round(b);
+      case 'DIVIDE':
+        if (Math.round(b) === 0) {
+          return 'Error';
+        } else {
+          return Math.round(a) / Math.round(b);
+        }
+      default:
+        throw TypeError;
+    }
+  },
+};
+
+module.exports = Utils;
